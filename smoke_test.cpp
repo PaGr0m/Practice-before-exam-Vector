d749b542b667610630643226692a44f459c96c63
Pavel Gromov <pagrom27@gmail.com>
2020-01-14 22:25:04 +0300

    Complete vector

--- smoke_test.cpp
+++ smoke_test.cpp
@@ -1,5 +1,6 @@
 //
 // Created by Александр Чори on 13.01.2020.
+// Changed by Pavel Gromov on 14.01.2020
 //
 
 #include <algorithm>
@@ -15,56 +16,173 @@
 #include "vector.hpp"
 
 namespace current_namespace {
-    using namespace std;
+    using namespace exam;
 }
 
 #define CUSTOM_ALLOCATOR_SUPPORT false
-#define RANDOM_ACCESS_ITERATOR_SUPPORT true
+#define RANDOM_ACCESS_ITERATOR_SUPPORT false
+
+void test_constructor() {
+    current_namespace::vector<int> vec1;
+    current_namespace::vector<int> vec2(10);
+
+    for (std::size_t i = 0; i < vec2.size(); ++i) {
+        assert(vec2[i] == 0);
+    }
+}
+
+void test_constructor_with_default_value() {
+    current_namespace::vector<int> fwd(10, 3);
+
+    for (std::size_t i = 0; i < fwd.size(); ++i) {
+        assert(fwd[i] == 3);
+    }
+}
+
+//     Should be compile error
+//void test_constructor_without_default() {
+//    struct NoDefaultConstructor {
+//        NoDefaultConstructor() = delete;
+//    };
+//    current_namespace::vector<NoDefaultConstructor> vec3(10);
+//}
+
+void test_screamer() {
+    struct Screamer {
+        Screamer() {
+            std::cout << "I was born" << std::endl;
+        }
+
+        virtual ~Screamer() {
+            std::cout << "I died" << std::endl;
+        }
+    };
+
+    current_namespace::vector<Screamer> arr;
+
+    for (std::size_t i = 0; i < 10; ++i) {
+        arr.push_back({});
+    }
+}
+
+void test_erase() {
+    std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::cout << vec.capacity() << std::endl;
+    std::cout << vec.size() << std::endl;
+    vec.erase(vec.begin());
+    vec.erase(vec.begin());
+
+    std::cout << vec.capacity() << std::endl;
+}
+
+void test_A() {
+    struct A {
+        int a;
+
+        A(int v) : a(v) {}
+
+        ~A() {
+            std::cout << a << std::endl;
+        }
+    };
+
+    std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::cout << " -------------- " << std::endl;
+    vec.erase(vec.begin());
+    std::cout << " -------------- " << std::endl;
+}
+
+void test_operator_equal() {
+    current_namespace::vector<double> vec1;
+    current_namespace::vector<double> vec2(15);
+
+    for (std::size_t i = 0; i < 15; ++i) {
+        vec2[i] = i * 0.1;
+    }
+
+    vec1 = vec2;
+
+    for (std::size_t i = 0; i < 15; ++i) {
+        assert(std::abs(vec1[i] - vec2[i]) < std::numeric_limits<double>::epsilon());
+    }
+
+    current_namespace::vector<double> vec3(vec1);
+
+    for (std::size_t i = 0; i < 15; ++i) {
+        assert(std::abs(vec1[i] - vec3[i]) < std::numeric_limits<double>::epsilon());
+    }
+}
+
+void test_size_capacity() {
+    current_namespace::vector<std::vector<int>> arr;
+
+    assert(arr.size() == 0);
+    assert(arr.capacity() == 0);
+    assert(arr.empty());
+
+    for (std::size_t i = 0; i < 700; ++i) {
+        arr.push_back({});
+    }
+
+    std::size_t capacity = arr.capacity();
+
+    arr.clear();
+
+    assert(arr.capacity() == capacity);
+}
+
+void test_shrink_to_fit() {
+    current_namespace::vector<int> vec;
+
+    for (std::size_t i = 0; i < 10000; ++i) {
+        vec.push_back(i);
+    }
+
+    for (std::size_t i = 0; i < 500; ++i) {
+        vec.pop_back();
+    }
+
+    vec.shrink_to_fit();
+    assert(vec.size() == vec.capacity());
+
+    for (std::size_t i = 0; i < 9500; ++i) {
+        assert(vec[i] == int(i));
+    }
+}
+
+void test_at() {
+    current_namespace::vector<int> vec(100);
+
+    for (std::size_t i = 0; i < vec.size(); ++i) {
+        vec[i] = i;
+    }
+
+    for (std::size_t i = 0; i < vec.size(); ++i) {
+        assert(vec[i] == vec.at(i));
+    }
+}
+
+void test_swap() {
+    current_namespace::vector<int> fwd(10);
+    current_namespace::vector<int> bck(10);
+
+    for (std::size_t i = 0; i < fwd.size(); ++i) {
+        fwd[i] = i;
+    }
+
+    for (std::size_t i = bck.size(); i > 0; --i) {
+        bck[10 - i] = i;
+    }
+
+    swap(fwd, bck);
+
+    for (std::size_t i = 0; i < fwd.size(); ++i) {
+        assert(bck[i] == int(i));
+        assert(fwd[i] == int(10 - i));
+    }
+}
 
 int main() {
-    {
-        current_namespace::vector<int> vec1;
-        current_namespace::vector<int> vec2(10);
-
-        for (std::size_t i = 0; i < vec2.size(); ++i) {
-            assert(vec2[i] == 0);
-        }
-    }
-
-    {
-        current_namespace::vector<int> fwd(10, 3);
-
-        for (std::size_t i = 0; i < fwd.size(); ++i) {
-            assert(fwd[i] == 3);
-        }
-    }
-
-//    {
-//        struct NoDefaultConstructor {
-//            NoDefaultConstructor() = delete;
-//        };
-//
-//         current_namespace::vector<NoDefaultConstructor> vec3(10);
-//    }
-//     Should be compile error
-
-    {
-        struct Screamer {
-            Screamer() {
-                std::cout << "I was born" << std::endl;
-            }
-
-            virtual ~Screamer() {
-                std::cout << "I died" << std::endl;
-            }
-        };
-
-        current_namespace::vector<Screamer> arr;
-
-        for (std::size_t i = 0; i < 10; ++i) {
-            arr.push_back({});
-        }
-    }
 
 #if CUSTOM_ALLOCATOR_SUPPORT
     {
@@ -87,95 +205,6 @@ int main() {
     }
 #endif
 
-    {
-        current_namespace::vector<double> vec1;
-        current_namespace::vector<double> vec2(15);
-
-        for (std::size_t i = 0; i < 15; ++i) {
-            vec2[i] = i * 0.1;
-        }
-
-        vec1 = vec2;
-
-        for (std::size_t i = 0; i < 15; ++i) {
-            assert(abs(vec1[i] - vec2[i]) < std::numeric_limits<double>::epsilon());
-        }
-
-        current_namespace::vector<double> vec3(vec1);
-
-        for (std::size_t i = 0; i < 15; ++i) {
-            assert(abs(vec1[i] - vec3[i]) < std::numeric_limits<double>::epsilon());
-        }
-    }
-
-    {
-        current_namespace::vector<std::vector<int>> arr;
-
-        assert(arr.size() == 0);
-        assert(arr.capacity() == 0);
-        assert(arr.empty());
-
-        for (std::size_t i = 0; i < 700; ++i) {
-            arr.push_back({});
-        }
-
-        std::size_t capacity = arr.capacity();
-
-        arr.clear();
-
-        assert(arr.capacity() == capacity);
-    }
-
-    {
-        current_namespace::vector<int> vec;
-
-        for (std::size_t i = 0; i < 10000; ++i) {
-            vec.push_back(i);
-        }
-
-        for (std::size_t i = 0; i < 500; ++i) {
-            vec.pop_back();
-        }
-
-        vec.shrink_to_fit();
-        assert(vec.size() == vec.capacity());
-
-        for (std::size_t i = 0; i < 9500; ++i) {
-            assert(vec[i] == i);
-        }
-    }
-
-    {
-        current_namespace::vector<int> vec(100);
-
-        for (std::size_t i = 0; i < vec.size(); ++i) {
-            vec[i] = i;
-        }
-
-        for (std::size_t i = 0; i < vec.size(); ++i) {
-            assert(vec[i] == vec.at(i));
-        }
-    }
-
-    {
-        current_namespace::vector<int> fwd(10);
-        current_namespace::vector<int> bck(10);
-
-        for (std::size_t i = 0; i < fwd.size(); ++i) {
-            fwd[i] = i;
-        }
-
-        for (std::size_t i = bck.size(); i > 0; --i) {
-            bck[9 - i] = i;
-        }
-
-        swap(fwd, bck);
-
-        for (std::size_t i = 0; i < fwd.size(); ++i) {
-            assert(bck[i] == i);
-            assert(fwd[i] == 9 - i);
-        }
-    }
 
 #if RANDOM_ACCESS_ITERATOR_SUPPORT
     {
@@ -189,6 +218,14 @@ int main() {
         sort(std::begin(fwd), std::end(fwd));
     }
 #endif
+    test_constructor();
+    test_constructor_with_default_value();
+    test_screamer();
+    test_operator_equal();
+    test_size_capacity();
+    test_shrink_to_fit();
+    test_at();
+    test_swap();
 
     std::cout << "All tests have passed!" << std::endl;
 
--- vector.hpp
+++ vector.hpp
@@ -1,9 +1,256 @@
 //
-// Created by Александр Чори on 13.01.2020.
+// Created by Pavel Gromov on 14.01.2020.
 //
 
 #ifndef PRACTICE_BEFORE_EXAM_VECTOR_VECTOR_HPP
 #define PRACTICE_BEFORE_EXAM_VECTOR_VECTOR_HPP
 
+#include <exception>
+
+namespace exam {
+
+    template<typename Type>
+    struct vector {
+
+        using value_type = Type;
+        using size_type = size_t;
+
+        vector() : data_(nullptr),
+                   size_(0),
+                   capacity_(0) {}
+
+        explicit vector(size_t size) : size_(size), capacity_(size) {
+            data_ = new Type[size_]{};
+        }
+
+        vector(size_t size, Type num) : size_(size), capacity_(size) {
+            data_ = new Type[size_];
+
+            for (size_t i = 0; i < size; ++i)
+                data_[i] = num;
+        }
+
+        vector(const vector<Type> &other)
+                : size_(other.size_),
+                  capacity_(other.capacity_) {
+            data_ = new Type[capacity_]{};
+            for (size_t i = 0; i < size_; ++i) {
+                data_[i] = other.data_[i];
+            }
+        }
+
+        ~vector() {
+            delete[] data_;
+        }
+
+        vector<Type> &operator=(vector<Type> other) {
+            swap(other);
+            return *this;
+        }
+
+        void swap(vector<Type> &other) {
+            std::swap(data_, other.data_);
+            std::swap(size_, other.size_);
+            std::swap(capacity_, other.capacity_);
+        }
+
+        Type *data() {
+            return data_;
+        }
+
+        const Type *data() const {
+            return data_;
+        }
+
+        bool empty() const {
+            return size_ == 0;
+        }
+
+        size_t size() const {
+            return size_;
+        }
+
+        size_t capacity() const {
+            return capacity_;
+        }
+
+        Type &operator[](size_t idx) {
+            return data_[idx];
+        }
+
+        const Type &operator[](size_t idx) const {
+            return data_[idx];
+        }
+
+        const Type &at(size_t idx) const {
+            if (idx > size_ || size_ == 0)
+                throw std::out_of_range("Index out of range");
+
+            return data_[idx];
+        }
+
+        Type &at(size_t idx) {
+            return const_cast<Type &>(std::as_const(*this).at(idx));
+        }
+
+        const Type &front() const {
+            return at(0);
+        }
+
+        Type &front() {
+            return const_cast<Type &>(std::as_const(*this).front());
+        }
+
+        const Type &back() const {
+            return at(size_ - 1);
+        }
+
+        Type &back() {
+            return const_cast<Type &>(std::as_const(*this).back());
+        }
+
+        void reserve(size_t new_capacity) {
+            if (capacity_ >= new_capacity)
+                return;
+
+            relocate(size_, new_capacity);
+        }
+
+        void shrink_to_fit() {
+            if (capacity_ == size_)
+                return;
+
+            relocate(size_, size_);
+        }
+
+        void clear() {
+            delete[] data_;
+
+            data_ = new Type[capacity_]{};
+            size_ = 0;
+        }
+
+        void insert(size_t idx, Type value) {
+            if (idx >= size_)
+                throw std::out_of_range("Index out of range (insert)");
+
+            if (size_ + 1 > capacity_) {
+                relocate(size_ + 1, next_capacity_value());
+
+                for (size_t i = size_ + 1; i > idx; --i) {
+                    data_[i + 1] = data_[i];
+                }
+
+                data_[idx] = value;
+
+                return;
+            }
+
+            for (size_t i = size_ + 1; i > idx; --i) {
+                data_[i + 1] = data_[i];
+            }
+
+            data_[idx] = value;
+            ++size_;
+        }
+
+        void erase(size_t idx) {
+            if (idx >= size_)
+                throw std::out_of_range("Index out of range (erase)");
+
+            for (size_t i = idx; i < size_; ++i) {
+                data_[i] = data_[i + 1];
+            }
+
+            --size_;
+        }
+
+        void push_back(const Type &value) {
+            if (size_ + 1 > capacity_) {
+                relocate(size_ + 1, next_capacity_value());
+                data_[size_ - 1] = value;
+                return;
+            }
+
+            ++size_;
+            data_[size_ - 1] = value;
+        }
+
+        void pop_back() {
+            if (size_ == 0)
+                throw std::out_of_range("Index out of range (pop_back)");
+
+            --size_;
+        }
+
+        void resize(size_t new_size) {
+            if (capacity_ > new_size) {
+                capacity_ = new_size;
+                size_ = new_size;
+                return;
+            }
+
+            relocate(new_size, new_size);
+        }
+
+    private:
+        void relocate(size_t new_size, size_t new_capacity) {
+            Type *new_data = new Type[new_capacity]{};
+
+            for (size_t i = 0; i < size_; ++i)
+                new_data[i] = data_[i];
+
+            delete[] data_;
+
+            data_ = new_data;
+            size_ = new_size;
+            capacity_ = new_capacity;
+        }
+
+        size_t next_capacity_value() {
+            if (capacity_ == 0)
+                return 1;
+
+            return capacity_ * 2;
+        }
+
+        struct iterator : std::iterator<std::random_access_iterator_tag, Type> {
+            using pointer = typename std::iterator<std::random_access_iterator_tag, Type>::pointer;
+            using reference = typename std::iterator<std::random_access_iterator_tag, Type>::reference;
+            using difference_type = typename std::iterator<std::random_access_iterator_tag, Type>::difference_type;
+
+        private:
+            explicit iterator(vector<Type> *container)
+                    : container_(container),
+                      idx_(0) {}
+
+            iterator(vector<Type> *container, size_t idx)
+                    : container_(container),
+                      idx_(idx) {}
+
+        private:
+            vector<Type> &container_;
+            size_t idx_;
+        };
+
+    private:
+        Type *data_;
+
+        size_t size_;
+        size_t capacity_;
+    };
+
+    template<typename Type>
+    void swap(vector<Type> &lhs, vector<Type> &rhs) {
+        lhs.swap(rhs);
+    }
+
+    template<typename Type, typename Comparator>
+    bool compare(const vector<Type> &lhs, const vector<Type> &rhs, Comparator comparator) {
+        return std::lexicographical_compare(lhs.data(), lhs.data() + lhs.size(),
+                                            rhs.data(), rhs.data() + rhs.size(),
+                                            comparator);
+    }
+}
 
 #endif //PRACTICE_BEFORE_EXAM_VECTOR_VECTOR_HPP
